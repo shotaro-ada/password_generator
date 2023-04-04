54a0208980761fbe3a071e362a2affe1f65e69f8
shotaro-ada <syouta2223505@gmail.com>
2023-04-04 23:53:34 +0900

    new option

--- passwdgen.c
+++ passwdgen.c
@@ -9,28 +9,38 @@
 
 int getRandomChar(int min, int max);
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 	int passlen = MAX_SIZE - 1;
 	int opt;
-	while ((opt = getopt(argc, argv, "l:n")) != -1) {
-		switch (opt) {
-			case 'l':
-				if (atoi(optarg) == 0 || atoi(optarg) >= MAX_SIZE) {
-					fprintf(stderr, "Error: Invaliid password length.\n");
-					return 1;
-				}
-				passlen = atoi(optarg);
-				break;
-			case 'n':
-				exit(0);
-			default:
-				fprintf(stderr, "invalid option\n");
+	while ((opt = getopt(argc, argv, "l:n:")) != -1)
+	{
+		switch (opt)
+		{
+		case 'l':
+			if (atoi(optarg) == 0 || atoi(optarg) >= MAX_SIZE)
+			{
+				fprintf(stderr, "Error: Invaliid password length.\n");
 				return 1;
+			}
+			passlen = atoi(optarg);
+			break;
+		case 'n':
+			if (!optarg)
+			{
+				fprintf(stderr, "Error: Invalid option argument.\n");
+				return 1;
+			}
+			// exit(0);
+		default:
+			fprintf(stderr, "invalid option\n");
+			return 1;
 		}
 	}
 
-	char *words = (char*) malloc((passlen + 1) * sizeof(char));
-	if(words == NULL){
+	char *words = (char *)malloc((passlen + 1) * sizeof(char));
+	if (words == NULL)
+	{
 		fprintf(stderr, "Error: Memory allocation failed.\n");
 		return 1;
 	}
@@ -38,11 +48,15 @@ int main(int argc, char *argv[]){
 	time_t t;
 	srandom(time(&t));
 	int i;
-	for(i = 0; i < passlen; i++){
-		int c = getRandomChar(MIN_CHAR, MAX_CHAR);	
-		if(c == '\\'){
-			i--;	
-		} else {
+	for (i = 0; i < passlen; i++)
+	{
+		int c = getRandomChar(MIN_CHAR, MAX_CHAR);
+		if (c == '\\')
+		{
+			i--;
+		}
+		else
+		{
 			words[i] = (char)c;
 		}
 	}
@@ -52,11 +66,8 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
-
-int getRandomChar(int min, int max){
+int getRandomChar(int min, int max)
+{
 	int num = (int)((double)random() * (max + 1 - min) / (double)RAND_MAX + min);
 	return num;
 }
-
-
-// アルゴリズムが未完成
