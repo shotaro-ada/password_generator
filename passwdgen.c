9eb2f40d4dc5b72bfbb75ec7d5171ad0b6df0262
shotaro-ada <syouta2223505@gmail.com>
2023-04-05 20:42:13 +0900

    change algorythm of reading options

--- passwdgen.c
+++ passwdgen.c
@@ -11,32 +11,40 @@ int getRandomChar(int min, int max);
 
 int main(int argc, char *argv[])
 {
+
 	int passlen = MAX_SIZE - 1;
-	int opt;
-	while ((opt = getopt(argc, argv, "l:n:")) != -1)
+	// int opt;
+	// while ((opt = getopt(argc, argv, "ln:")) != -1)
+	// {
+	// 	switch (opt)
+	// 	{
+	// 	case 'l':
+	// 		if (atoi(optarg) == 0 || atoi(optarg) >= MAX_SIZE)
+	// 		{
+	// 			fprintf(stderr, "Error: Invaliid password length.\n");
+	// 			return 1;
+	// 		}
+	// 		passlen = atoi(optarg);
+	// 		// break;
+	// 	case 'n':
+	// 		if (!optarg)
+	// 		{
+	// 			fprintf(stderr, "Error: Invalid option argument.\n");
+	// 			return 1;
+	// 		}
+	// 		// exit(0);
+	// 	default:
+	// 		fprintf(stderr, "invalid option\n");
+	// 		return 1;
+	// 	}
+	// }
+	int i;
+	for (i = 0; i < argc; i++)
 	{
-		switch (opt)
-		{
-		case 'l':
-			if (atoi(optarg) == 0 || atoi(optarg) >= MAX_SIZE)
-			{
-				fprintf(stderr, "Error: Invaliid password length.\n");
-				return 1;
-			}
-			passlen = atoi(optarg);
-			break;
-		case 'n':
-			if (!optarg)
-			{
-				fprintf(stderr, "Error: Invalid option argument.\n");
-				return 1;
-			}
-			// exit(0);
-		default:
-			fprintf(stderr, "invalid option\n");
-			return 1;
-		}
+		const char *arg = argv[i];
 	}
+
+	printf("%d\n", argc);
 
 	char *words = (char *)malloc((passlen + 1) * sizeof(char));
 	if (words == NULL)
@@ -47,7 +55,6 @@ int main(int argc, char *argv[])
 
 	time_t t;
 	srandom(time(&t));
-	int i;
 	for (i = 0; i < passlen; i++)
 	{
 		int c = getRandomChar(MIN_CHAR, MAX_CHAR);
